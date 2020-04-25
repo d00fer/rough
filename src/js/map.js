a8f494d55af666cb687abdbca3e74c946af4032f
Patrycjusz Myszka <pat.myszka@gmail.com>
2020-04-25 23:35:26 +0100

    cyk

--- src/js/map.js
+++ src/js/map.js
@@ -1,10 +1,17 @@
-let generateLevel = () => {
-  generateTiles()
+function generateLevel() {
+  tryTo('generate map', function () {
+    return generateTiles() == randomPassableTile().getConnectedTiles().length
+  })
+
+  generateMonsters()
+
+  for (let i = 0; i < 3; i++) {
+    randomPassableTile().treasure = true
+  }
 }
-//come back here if do not working
-//let tiles = [] wont work don't konow why
-//gowno jedno
-const generateTiles = () => {
+
+function generateTiles() {
+  let passableTiles = 0
   tiles = []
   for (let i = 0; i < numTiles; i++) {
     tiles[i] = []
@@ -13,19 +20,46 @@ const generateTiles = () => {
         tiles[i][j] = new Wall(i, j)
       } else {
         tiles[i][j] = new Floor(i, j)
+        passableTiles++
       }
     }
   }
+  return passableTiles
 }
 
-const inBounds = (x, y) => {
-  return x > 0 && y > 0 && x < numTiles - 1, y < numTiles - 1
+function inBounds(x, y) {
+  return x > 0 && y > 0 && x < numTiles - 1 && y < numTiles - 1
 }
 
-const getTile = (x, y) => {
+function getTile(x, y) {
   if (inBounds(x, y)) {
     return tiles[x][y]
   } else {
     return new Wall(x, y)
   }
+}
+
+function randomPassableTile() {
+  let tile
+  tryTo('get random passable tile', function () {
+    let x = randomRange(0, numTiles - 1)
+    let y = randomRange(0, numTiles - 1)
+    tile = getTile(x, y)
+    return tile.passable && !tile.monster
+  })
+  return tile
+}
+
+function generateMonsters() {
+  monsters = []
+  let numMonsters = level + 1
+  for (let i = 0; i < numMonsters; i++) {
+    spawnMonster()
+  }
+}
+
+function spawnMonster() {
+  let monsterType = shuffle([Bird, Snake, Tank, Eater, Jester])[0]
+  let monster = new monsterType(randomPassableTile())
+  monsters.push(monster)
 }
